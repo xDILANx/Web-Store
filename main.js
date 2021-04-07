de084cdadbf0925283b2310ba0b49e6cfde82f1c
DEVS <estebanvilladasalazar@gmail.com>
2021-04-07 14:47:48 -0500

    PRIMER COMMIT D.V

---
+++ main.js
@@ -0,0 +1,58 @@
+const fa = document.querySelector(".fa");
+const fas = document.querySelector(".fas");
+
+
+fa.addEventListener("click", function () {
+  document.getElementById("menu-lateral").classList.toggle("abrir");
+  document.getElementById("logo").classList.toggle("moveRight");
+});
+
+fas.addEventListener("click", function () {
+  document.getElementById("shoppingCart").classList.toggle("open");
+  document.getElementById("logo").classList.toggle("moveLeft");
+});
+
+
+function ajax(url){
+    const xhttp = new XMLHttpRequest();
+    //const url = 'productsWoman.html';
+    xhttp.onreadystatechange = function(){
+        if (this.readyState == 4 && this.status == 200){
+            console.log(this.responseText);
+            document.getElementById("products").innerHTML = this.responseText;
+  }
+}
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
+const url = 'productsWoman.html';
+document.getElementById("woman").addEventListener("click",function(){
+  ajax(url);
+});
+
+const url1 = 'productsMan.html';
+document.getElementById("man").addEventListener("click",function(){
+  ajax(url1);
+});
+
+const url2 = 'productsKid.html';
+document.getElementById("kid").addEventListener("click",function(){
+  ajax(url2);
+});
+
+
+/*function ajax(){
+  const xhttp = new XMLHttpRequest();
+  const url = 'productsWoman.html';
+
+  xhttp.onreadystatechange = function(){
+      if (this.readyState == 4 && this.status == 200){
+          console.log(this.responseText);
+          document.getElementById("products").innerHTML = this.responseText;
+}
+}
+xhttp.open("GET", url, true);
+xhttp.send();
+}*/
+
+
